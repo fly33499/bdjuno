c9f2dca29d3bb0397fb541db178426b0e07ff621
Donghoon Kim <fly33499@gmail.com>
2021-09-07 21:14:07 +0900

    add nft parser

---
+++ cmd/bdjuno/firmachain_nft.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	nfttypes "github.com/firmachain/firmachain/x/nft/types"
+)
+
+func FirmaChainNFTMessagesParser(_ codec.Marshaler, cosmosMsg sdk.Msg) ([]string, error) {
+	switch msg := cosmosMsg.(type) {
+	case *nfttypes.MsgMint:
+		return []string{msg.Owner}, nil
+
+	case *nfttypes.MsgBurn:
+		return []string{msg.Owner}, nil
+
+	case *nfttypes.MsgTransfer:
+		return []string{msg.Owner, msg.ToAddress}, nil
+	}
+
+	return nil, MessageNotSupported(cosmosMsg)
+}
