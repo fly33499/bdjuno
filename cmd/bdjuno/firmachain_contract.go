04631c3fd5768bb894c96b56fa207b780c3057f2
Donghoon Kim <fly33499@gmail.com>
2021-09-07 21:14:01 +0900

    add contract parser

---
+++ cmd/bdjuno/firmachain_contract.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	contracttypes "github.com/firmachain/firmachain/x/contract/types"
+)
+
+func FirmaChainContractMessagesParser(_ codec.Marshaler, cosmosMsg sdk.Msg) ([]string, error) {
+	switch msg := cosmosMsg.(type) {
+	case *contracttypes.MsgAddContractLog:
+		return []string{msg.Creator, msg.OwnerAddress}, nil
+
+	case *contracttypes.MsgCreateContractFile:
+		var addresses []string
+		addresses = append(addresses, msg.Creator)
+		addresses = append(addresses, msg.OwnerList...)
+
+		return addresses, nil
+	}
+
+	return nil, MessageNotSupported(cosmosMsg)
+}
